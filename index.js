2c8a0232e0a764e37db749fe19a3be716e3db317
rukiii4444 <haruki14rezer@gmail.com>
2025-02-07 19:14:12 +0900

    add messages

--- index.js
+++ index.js
@@ -11,28 +11,37 @@ const CHANNEL_ID = process.env.CHANNEL_ID;
 const START_DATE = new Date(process.env.START_DATE);
 
 client.once('ready', () => {
-    console.log(`✅ Bot is online:): ${client.user.tag}`);
+    console.log(`✅ Bot is online:)`);
 
     cron.schedule('15 7 * * *', async () => {  
         const today = new Date();
         const diffTime = today - START_DATE;
         const dayCount = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+        const messages = [
+            process.env.MESSAGE_1.replace("{dayCount}", dayCount),
+            process.env.MESSAGE_2,
+            process.env.MESSAGE_3
+        ];
 
         try {
-            const channel = await client.channels.fetch(CHANNEL_ID); // ✅ 修正
+            const channel = await client.channels.fetch(CHANNEL_ID); 
 
             if (channel) {
-                await channel.send(`今日はニート **${dayCount}**日目 `);
+
+                for (const msg of messages) {
+                    await channel.send(msg);
+                }
+
                 console.log(`📢 メッセージを送信しました: ${dayCount}日目`);
             } else {
                 console.log("⚠️ チャンネルが見つかりません！");
             }
         } catch (error) {
-            console.error("❌ チャンネル取得エラー:", error);
+            console.error("😔 チャンネル取得エラー:", error);
         }
     });
 
-    console.log("⏰ Setting done;)");
+    console.log("👍 Setting done;)");
 });
 
 client.login(TOKEN);
